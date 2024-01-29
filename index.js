7a0ed996c91b8199b51185f1b8c6a90a623f7690
Muzhdah26 <149178374+Muzhdah26@users.noreply.github.com>
2024-01-29 14:43:32 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const logger = require('morgan');
+// const logger = require('morgan');
 const axios = require('axios')
 const cors = require('cors')
 require("dotenv").config();
